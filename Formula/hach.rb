b694af9d4b8a79c03f47cd5b9006d2e1b706dada
Jonathan Baldie <jon@jonbaldie.com>
2026-09-06 13:31:00 +0100

    feat: add on_linux block to hach formula

--- Formula/hach.rb
+++ Formula/hach.rb
@@ -16,6 +16,11 @@ class Hach < Formula
     end
   end
 
+  on_linux do
+    url "https://github.com/jonbaldie/hach/releases/download/v0.1.0/hach_0.1.0_linux_amd64.tar.gz"
+    sha256 "51e65f2ecf3b033ede530e822b9c89142639da4d7b8a3e3b9cb84d052e01dfd5"
+  end
+
   def install
     bin.install "hach"
     prefix.install "LICENSE"
