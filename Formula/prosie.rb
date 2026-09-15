43bffd02d30d030e210f7e7b3e8b7eb0bf32b9aa
Jonathan Baldie <jon@jonbaldie.com>
2026-09-15 14:32:38 +0100

    prosie 0.2.0
    - generate continue: --instruction, --words, --lines
    - llm show / models / update commands

--- Formula/prosie.rb
+++ Formula/prosie.rb
@@ -6,25 +6,25 @@ class Prosie < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.1.2/prosie_0.1.2_darwin_arm64.tar.gz"
-      sha256 "c7fc0039a91836fdbbcaa4770e3943f66b0c8ca49d5ab4957277803164a3303a"
+      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.2.0/prosie_0.2.0_darwin_arm64.tar.gz"
+      sha256 "91692e82784199c46abb3aaf180ae4b23721e08173662ba35b75ed74d8e095e0"
     end
 
     on_intel do
-      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.1.2/prosie_0.1.2_darwin_amd64.tar.gz"
-      sha256 "2b79b3e0f37484a239e25ef22d68b88dafd7d43778646205a1b2d9198acab96a"
+      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.2.0/prosie_0.2.0_darwin_amd64.tar.gz"
+      sha256 "2a423e2bd9566f5936bd31ddb500e646f7bdafdbc22c80cbb5a2d7605bb5a012"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.1.2/prosie_0.1.2_linux_arm64.tar.gz"
-      sha256 "388ce0b8b433605896f17a34e534b0f4baaa2287f9f27bbbcb4cbd7567dec0ea"
+      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.2.0/prosie_0.2.0_linux_arm64.tar.gz"
+      sha256 "8e62c7da9014bb3d922640e4654e38f102a2c9f7a78db41f18b0e4433b0dcb5c"
     end
 
     on_intel do
-      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.1.2/prosie_0.1.2_linux_amd64.tar.gz"
-      sha256 "58c333a1a820fd08c59f6e7292d6d2d281c43f130d58b7b8ea5f08f2eb23329b"
+      url "https://github.com/jonbaldie/prosie-cli/releases/download/v0.2.0/prosie_0.2.0_linux_amd64.tar.gz"
+      sha256 "9b88243b5e0a69bd4a854ebdb47749b96c4269e07ae6bd540289cccdd67da232"
     end
   end
 
@@ -34,6 +34,6 @@ class Prosie < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/prosie version")
+    assert_match "0.2.0", shell_output("#{bin}/prosie version")
   end
 end
